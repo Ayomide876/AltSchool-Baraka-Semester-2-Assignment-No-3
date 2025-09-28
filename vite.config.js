31c888c19c64128c1f6a7c734fd1ddf3b5c5b367
Ayomide <ayomidelaw01@gmail.com>
2025-09-28 22:59:46 +0100

    Small changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AltSchool-Baraka-Semester-2-Assignment-No-3/'
 })
