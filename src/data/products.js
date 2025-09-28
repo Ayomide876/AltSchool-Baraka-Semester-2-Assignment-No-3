bb6065cdab9f37acb7a687e79e5ec9a74b0a4211
Ayomide <ayomidelaw01@gmail.com>
2025-09-28 23:06:22 +0100

    Small changes

--- src/data/products.js
+++ src/data/products.js
@@ -4,42 +4,42 @@ const products = [
     name: "Red Bench",
     category: "People",
     price: 3.89,
-    image: "/assets/images/red-heart-balloons.jpg"
+    image: "assets/images/red-heart-balloons.jpg"
   },
   {
     id: 2,
     name: "Sahara",
     category: "Places",
     price: 12.5,
-    image: "/assets/images/Sahara.jpg"
+    image: "assets/images/Sahara.jpg"
   },
   {
     id: 3,
     name:"Stonehedge",
     category: "Places",
     price: 12.5,
-    image:"/assets/images/stonehedge.jpg",
+    image:"assets/images/stonehedge.jpg",
   },
   {
     id: 4,
     name:"Red Bench",
     category: "Places",
     price: 12.5,
-    image:"/assets/images/yellow-balloon.jpg",
+    image:"assets/images/yellow-balloon.jpg",
   },
   {
     id: 5,
     name:"Red Bench",
     category: "Places",
     price: 12.5,
-    image:"/assets/images/yellow-wall.jpg",
+    image:"assets/images/yellow-wall.jpg",
   },
   {
     id: 6,
     name:"Stonehedge",
     category: "Places",
     price: 12.5,
-    image:"/assets/images/stonehedge.jpg",
+    image:"assets/images/stonehedge.jpg",
   }
 
 ];
